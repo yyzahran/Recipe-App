f82e66f2693ac1881241b6f3707eda952dd3beb8
Youssef Zahran <youssefzahran@MacBook-Pro.hitronhub.home>
2022-11-07 20:28:48 -0700

    configure docker-compsoe to wait_for_db

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -21,11 +21,12 @@ class CommandTests(SimpleTestCase):
         patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
-    def test_wait_for_db_delay(self, patched_sleep,patched_check):
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test command waiting for db when gettings OperationalError"""
-        patched_check.side_effect = [Psycopg2Error] * 2 + [OperationalError] * 3 + [True]
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+            [OperationalError] * 3 + [True]
 
-        call_command('wait_for_db') 
+        call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)  # assert mocked check method call count is 6 times
 
