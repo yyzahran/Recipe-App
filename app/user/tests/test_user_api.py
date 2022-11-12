f13ab666d924c31c84e20741b2de610dd42d5d93
yyzahran <youssef_zahran@live.com>
2022-11-11 21:23:19 -0700

    fix assertion

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -172,5 +172,5 @@ class PrivateUserApiTests(TestCase):
 
         self.user.refresh_from_db()
         self.assertEqual(self.user.name, payload['name'])
-        self.assertEqual(self.user.password, payload['password'])
+        self.assertTrue(self.user.check_password(payload['password']))
         self.assertEqual(res.status_code, status.HTTP_200_OK)
