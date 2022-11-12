548aacf46cf40a40ffb505edf8a4dc119e607eb8
yyzahran <youssef_zahran@live.com>
2022-11-12 16:48:34 -0700

    rerun migrations

--- app/core/admin.py
+++ app/core/admin.py
@@ -34,6 +34,7 @@ class UserAdmin(BaseUserAdmin):
                 'email',
                 'password1',
                 'password2',
+                'name',
                 'is_active',
                 'is_staff',
                 'is_superuser',
