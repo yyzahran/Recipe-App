afb357520fa3f19dba84312d795f16b0fe2e7f41
Youssef Zahran <55523423+yyzahran@users.noreply.github.com>
2022-11-24 15:57:53 -0700

    Revert "Image api"

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,8 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
 
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -37,9 +35,3 @@ urlpatterns = [
     path('api/user/', include('user.urls')),
     path('api/recipe/', include('recipe.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT,
-    )
--- app/core/models.py
+++ app/core/models.py
@@ -1,22 +1,11 @@
 """Database models"""
 
-import uuid
-import os
-
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
     PermissionsMixin)
-
-
-def recipe_image_file_path(instance, filename):
-    """Generate file path for new recipe images"""
-    ext = os.path.splitext(filename)[1]
-    filename = f'{uuid.uuid4()}{ext}'
-
-    return os.path.join('uploads', 'recipe', filename)
 
 
 class UserManager(BaseUserManager):
@@ -63,7 +52,6 @@ class Recipe(models.Model):
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
     ingredients = models.ManyToManyField('ingredient')
-    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
