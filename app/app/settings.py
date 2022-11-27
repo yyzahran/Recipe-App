895e0b624e7c80f03b43f5139f632098ef414cc6
yyzahran <youssef_zahran@live.com>
2022-11-26 23:05:45 -0700

    fix for checks

--- app/app/settings.py
+++ app/app/settings.py
@@ -129,8 +129,8 @@ USE_TZ = True
 STATIC_URL = '/static/static/'
 MEDIA_URL = '/static/media/'
 
-MEDIA_ROOT = 'vol/web/media'
-STATIC_ROOT = 'vol/web/static'
+MEDIA_ROOT = '/vol/web/media'
+STATIC_ROOT = '/vol/web/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
